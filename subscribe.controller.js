c99819efd98d6f84eafcbe42ff4abefed376fe71
Joe Greaser <jgreaser@flvs.net>
2016-09-12 09:48:02 -0400

    First Commit

---
+++ subscribe.controller.js
@@ -0,0 +1,37 @@
+angular
+    .module('app')
+    .controller('subscribeController', subscribeController);
+
+subscribeController.$inject = ['$rootScope', '$scope', '$element',  '$compile', 'mailService'];
+
+function subscribeController($rootscope, $scope, $element,  $compile, mailService) {
+
+    var vm = this;
+    vm.hide = false;
+    vm.firstName = 'First';
+    vm.lastName = 'Last';
+    vm.email = 'email';
+
+    vm.subscribed = false;
+    vm.processing = false;
+    vm.subscribe = subscribe;
+
+    $scope.$watch(mailService.getSubscribedStatus, function(newValue, oldValue) {
+          console.log("subscibed changed!");
+          console.log("it is now " + newValue);
+          vm.subscribed = mailService.getSubscribedStatus();
+          vm.processing = false;
+        });
+
+
+
+    function subscribe (first, last, email){
+
+        console.log("SUBSCRIBE! (controller)");
+vm.processing = true;
+        mailService.subscribe(first, last, email);
+    }
+
+   
+    
+}
---
+++ subscribe.service.js
@@ -0,0 +1,41 @@
+angular
+    .module('app')
+    .factory('mailService', mailService);
+
+mailService.$inject = ['$http'];
+
+
+function mailService($http) {
+  
+    var subscribed = false;
+
+    var service = {
+        subscribe: subscribe,
+        getSubscribedStatus: getSubscribedStatus
+    };
+
+    return service;
+
+    function subscribe(first, last, email) {
+
+        console.log("SUBSCRIBE - SERVICE");
+
+        $http.get("https://reinvention.flvs.net/fancygraf/php/mailto.php?&first=" + first + "&last=" + last + "&email=" + email)
+            .then(function(response) {
+            console.log(response);
+            subscribed = true;
+
+            }, function errorCallback(response) {
+            console.log(response);
+            subscribed = false;
+            });
+
+    };
+
+    function getSubscribedStatus (){
+        return subscribed;
+    }
+
+
+  
+}
